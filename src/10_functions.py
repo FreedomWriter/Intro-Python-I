e6441d6032c23a4c59cf12eb3ba190ca0c529fc7
FreedomWriter <natalienetworks@gmail.com>
2020-07-04 22:00:45 -0500

    functions

--- src/10_functions.py
+++ src/10_functions.py
@@ -6,6 +6,27 @@
 num = input("Enter a number: ")
 num = int(num)
 
+def is_even(number):
+    while True:
+        try:
+            if number == 0:
+                print('Is zero even or odd???')
+                break
+            else:
+                if not number % 2:
+                    print("Even!")
+                    break
+                else:
+                    print('Odd')
+                    break
+        except ValueError:
+            print('Please enter a number')
+            continue
+        except TypeError:
+            print('Please enter a number')
+
+is_even(num)
+
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
