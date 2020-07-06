da4f641e1b433b7213bc8814ee5fd59c5f6ed822
FreedomWriter <natalienetworks@gmail.com>
2020-07-06 11:38:21 -0500

    args

--- src/11_args.py
+++ src/11_args.py
@@ -23,16 +23,13 @@ def f2(*args):
                 temp = int(ar) + temp
             return temp
         except ValueError:
-            print("ValueError - Please provide a number, a dictionary of numbers, or a list of numbers")
-            break
+            return "ValueError - Please provide a number, a dictionary of numbers, or a list of numbers"
         except TypeError:
-            # try:
-            li = list(args[0])
-            print(sum(li))
-            break
-            # except TypeError:
-            #     print("TypeError - Please provide a number")
-            #     break
+            try:
+                li = list(args[0])
+                return sum(li)
+            except TypeError:
+                return "TypeError - Please provide a number"
 
 
 # print(f2(1))                    # Should print 1
@@ -45,8 +42,8 @@ a = [7, 6, 5, 4]
 b = {7, 6, 5, 4}
 
 # # How do you have to modify the f2 call below to make this work?
-print(f2(a))    # Should print 22
-print(f2(b))    # Should print 22
+# print(f2(a))    # Should print 22
+# print(f2(b))    # Should print 22
 
 # # Write a function f3 that accepts either one or two arguments. If one argument,
 # # it returns that value plus 1. If two arguments, it returns the sum of the
@@ -55,35 +52,45 @@ print(f2(b))    # Should print 22
 
 # # YOUR CODE HERE
 
+# def f3(num1, num2 = 1):
+#     return num1 + num2
+
 # print(f3(1, 2))  # Should print 3
 # print(f3(8))     # Should print 9
 
 
-# # Write a function f4 that accepts an arbitrary number of keyword arguments and
-# # prints out the keys and values like so:
-# #
-# # key: foo, value: bar
-# # key: baz, value: 12
-# #
-# # Note: Google "python keyword arguments".
+# Write a function f4 that accepts an arbitrary number of keyword arguments and
+# prints out the keys and values like so:
+
+# key: foo, value: bar
+# key: baz, value: 12
+
+# Note: Google "python keyword arguments".
+
+def f4(*args,**kwargs):
+    for arg in kwargs:
+        print(f"key: {arg}, value: {kwargs[arg]}")
+    for arg in args:
+        for el in arg:
+            print(f"key: {el}, value: {arg[el]}")
 
 # # YOUR CODE HERE
 
 # # Should print
 # # key: a, value: 12
 # # key: b, value: 30
-# f4(a=12, b=30)
+f4(a=12, b=30)
 
-# # Should print
-# # key: city, value: Berkeley
-# # key: population, value: 121240
-# # key: founded, value: "March 23, 1868"
+# # # Should print
+# # # key: city, value: Berkeley
+# # # key: population, value: 121240
+# # # key: founded, value: "March 23, 1868"
 # f4(city="Berkeley", population=121240, founded="March 23, 1868")
 
-# d = {
-#     "monster": "goblin",
-#     "hp": 3
-# }
+d = {
+    "monster": "goblin",
+    "hp": 3
+}
 
 # # How do you have to modify the f4 call below to make this work?
-# f4(d)
+f4(d)
